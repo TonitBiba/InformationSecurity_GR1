102d93912caba5e7fc060957f17248bbd787224c
TonitBiba <tonit.biba@hotmail.com>
2019-10-28 19:03:17 +0100

    V1.0.1

---
+++ InformationSecurity_GR1/Classes/StatisticsCondition.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InformationSecurity_GR1.Classes
+{
+    public class StatisticsCondition
+    {
+        public int conditionNumber { get; set; }
+        public int sign { get; set; }
+        public string[] condition { get; set; }
+    }
+}
